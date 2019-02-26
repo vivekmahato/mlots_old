9e6686c92e5fb629c884f435b0b6dc91bdc213d6
vivekmahato <vivekmahato@live.com>
2019-02-26 23:42:49 +0000

    addition of .gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='mlots',
-    version='0.0.0-test1',
+    version='0.0.0.a2',
     author="Vivek Mahato",
     author_email="vivek.mahato@ucdconnect.ie",
     description="Machine Learning Over Time-Series: A toolkit for time-series analysis",
@@ -13,9 +13,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/vivekmahato/mlots',
     packages=setuptools.find_packages(),
-    install_requires=[
-        'pandas', 'numpy', 'saxpy', 'editdistance'
-    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
